4047dc9d02b16393b6c56c0234a5191a38e0778a
kawsarmahmudsagor <kawsarmahmudsagor@gmail.com>
2025-11-20 17:49:13 +0600

    latest dataset training

--- src/dataset.py
+++ src/dataset.py
@@ -2,8 +2,9 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="V7gbNKr6N2UZG7FvzG6H")
 project = rf.workspace("xr23").project("foot-landmark-detection-bhwqm")
-version = project.version(5)
+version = project.version(6)
 dataset = version.download("yolov8")
+                
                 
                 
                 
--- src/training.py
+++ src/training.py
@@ -5,4 +5,4 @@ model = YOLO("yolo11n-pose.yaml")  # build a new model from YAML
 model = YOLO("yolo11n-pose.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data="/home/sagor/Projects/YOLO/src/Foot-Landmark-Detection-5/data.yaml", epochs=250, imgsz=320)
+results = model.train(data="/home/sagor/Projects/YOLO/src/Foot-Landmark-Detection-6/data.yaml", epochs=250, imgsz=320)
